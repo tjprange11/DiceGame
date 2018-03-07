9852f514f80ad49d504345323acf01b6cfe63484
TJ Prange <tjprange11@gmail.com>
2018-03-07 13:13:19 -0600

    advanced function and better functions

--- DiceGame.js
+++ DiceGame.js
@@ -1,25 +1,31 @@
 "use strict";
 
 	alert("Lets play some Hockey!!!");
-	let difficulty = prompt(" What would you like your difficulty to be? ( Hard(0) , Medium(1) , Easy(2) )");
 	let playerScore = 0, cpuScore = 0;
 	alert("Alrighty! First to five wins the game!");
+	
 	while(playerScore !== 5 && cpuScore !== 5){
-		if(faceOff()){
-			if(scoreGoal()){
+		if(faceOff(prompt("Guess a number in order to win the faceoff: (1 - 3)"))){
+			
+			if(checkIfGoalScored(prompt("Guess a number in order to score a goal: (1 - 12)"))){
 				alert("You scored!!!")
 				playerScore++;
 			}
 		}
+		
 		else{
-			if(bodyCheck()){
-				if(scoreGoal()){
+			
+			if(checkBodyCheck(prompt("Guess a number in order to bodycheck the other player: (1 - 6)"))){
+			
+				if(checkIfGoalScored(prompt("Guess a number in order to score a goal: (1 - 12)"))){
 					alert("You scored!!!")
 					playerScore++;
 				}
 			}
+			
 			else{
-				if(!makeSave()){
+			
+				if(!checkIfSaveMade(prompt("Guess a number in order to make the save: (1 - 4)"))){
 					alert("The CPU scored!!!")
 					cpuScore++;
 				}
@@ -27,32 +33,30 @@
 		}
 	}
 	
-function scoreGoal(){
-	let num = rollDie(12)
-	let guess = prompt("Guess a number in order to score a goal: (1 - 12)");
-	if(Math.abs(guess - num)<=difficulty){
-		return true;
-	}
-	else{
-		return false;
-	}
-}
-
-function makeSave(){
-	let num = rollDie(6)
-	let guess = prompt("Guess a number in order to make the save: (1 - 6)");
-	if(Math.abs(guess - num)<=difficulty){
-		return true;
-	}
-	else{
-		return false;
-	}
-}
-
-function bodyCheck(){
+function checkReboundGoal(guessRebound){
 	let num = rollDie(8)
-	let guess = prompt("Guess a number in order to bodycheck the other player: (1 - 8)");
-	if(Math.abs(guess - num)<=difficulty){
+	if(Math.abs(guessRebound - num) === 0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+	
+function checkIfGoalScored(guessGoal){
+	let num = rollDie(12);
+	if(Math.abs(guessGoal - num) === 0){
+		return true;
+	}
+	else if(Math.abs(guessGoal - num) <= 2){
+		return checkReboundGoal(prompt("Guess a number in order to score a rebound goal: (1 - 8)"));
+	}
+	return false;
+}
+
+function checkIfSaveMade(guessSave){
+	let num = rollDie(4);
+	if(Math.abs(guessSave - num) === 0){
 		return true;
 	}
 	else{
@@ -60,15 +64,20 @@ function bodyCheck(){
 	}
 }
 
-function faceOff(){
-	let num = rollDie(4);
-	let guess = prompt("Guess a number in order to win the faceoff: (1 - 4)");
-	if(Math.abs(guess - num)<=difficulty){
+function checkBodyCheck(guessCheck){
+	let num = rollDie(6);
+	if(Math.abs(guessCheck - num) === 0){
 		return true;
 	}
-	else{
-		return false;
+	return false;
+}
+
+function faceOff(guessFaceOff){
+	let num = rollDie(3);
+	if(Math.abs(guessFaceOff - num) === 0){
+		return true;
 	}
+	return false;
 }
 
 function rollDie(numOfSides){
